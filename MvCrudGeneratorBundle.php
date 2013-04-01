d36413c961605d9ddac2de7bd4b8217c1ab48a7e
Michaël VEROUX <mveroux@orupaca.fr>
2013-04-01 16:46:59 +0200

    Generator Sensio skeleton

--- MvCrudGeneratorBundle.php
+++ MvCrudGeneratorBundle.php
@@ -4,10 +4,22 @@ namespace Mv\CrudGeneratorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+use Symfony\Component\Console\Application;
+use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
+use Symfony\Component\Filesystem\FileSystem;
+
 class MvCrudGeneratorBundle extends Bundle
 {
     public function getParent()
     {
         return 'SensioGeneratorBundle';
     }
+    
+    public function registerCommands(Application $application){
+        $crudCommand = $application->get('generate:doctrine:crud');
+        $generator = new DoctrineCrudGenerator(new FileSystem, __DIR__.'/Resources/skeleton/crud');
+        $crudCommand->setGenerator($generator);
+
+        parent::registerCommands($application);
+    }    
 }
