485230406614d9f638f7c5e7e1060fbd3a215093
Michaël VEROUX <mveroux@orupaca.fr>
2013-06-29 12:58:50 +0200

    Amélioration du CRUD généré

---
+++ Command/GenerateDoctrineCrudCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mv\CrudGeneratorBundle\Command;
+
+use Sensio\Bundle\GeneratorBundle\Command\GenerateDoctrineCrudCommand AS SensioGenerateDoctrineCrudCommand;
+
+/**
+ * Generates a CRUD for a Doctrine entity.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class GenerateDoctrineCrudCommand extends SensioGenerateDoctrineCrudCommand
+{
+    protected function getSkeletonDirs($bundle = null)
+    {
+        $skeletonDirs = array();
+
+        if (isset($bundle) && is_dir($dir = $bundle->getPath().'/Resources/SensioGeneratorBundle/skeleton')) {
+            $skeletonDirs[] = $dir;
+        }
+
+        if (is_dir($dir = $this->getContainer()->get('kernel')->getRootdir().'/Resources/SensioGeneratorBundle/skeleton')) {
+            $skeletonDirs[] = $dir;
+        }
+
+        $skeletonDirs[] = __DIR__.'/../Resources/skeleton';
+        $skeletonDirs[] = __DIR__.'/../Resources';
+
+        return $skeletonDirs;
+    }
+}
